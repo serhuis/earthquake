65eac0005be40f237f6cab5dbef153c96c09725b
Sergiy <serhuis.s@gmail.com>
2017-04-05 23:20:35 +0300

    +1

--- app/src/main/java/com/example/serhiy/earthquake/EarthQuakeListFragment.java
+++ app/src/main/java/com/example/serhiy/earthquake/EarthQuakeListFragment.java
@@ -2,12 +2,26 @@ package com.example.serhiy.earthquake;
 
 
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.widget.ArrayAdapter;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 
 /**
@@ -35,9 +49,50 @@ public class EarthQuakeListFragment extends ListFragment {
         // Required empty public constructor
     }
 
-    public void refreshEarthQuakes(){
+    public void refreshEarthQuakes() {
         //Get a XML
         URL url;
-    }
+        try {
+            String quakeFeed = getString(R.string.quake_feed);
+            url = new URL(quakeFeed);
 
+            URLConnection connecrion;
+            connecrion = url.openConnection();
+            HttpURLConnection httpConnection = (HttpURLConnection) connecrion;
+            int reaponceCode = httpConnection.getResponseCode();
+
+            if (reaponceCode == HttpURLConnection.HTTP_OK) {
+                InputStream in = httpConnection.getInputStream();
+
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+
+                Document dom = db.parse(in);
+                Element docElement = dom.getDocumentElement();
+
+                earthquakes.clear();
+
+                NodeList nl = docElement.getElementsByTagName("entry");
+                if (nl != null && nl.getLength() > 0) {
+                    for (int i = 0; i < nl.getLength(); i++) {
+                        Element entry = (Element)nl.item(i);
+                        Element title = (Element)entry.getElementsByTagName("title").item(0);
+                        Element g = (Element)entry.getElementsByTagName("georss:point").item(0);
+                        Element when = (Element)entry.getElementsByTagName("updated").item(0);
+                        Element link = (Element)entry.getElementsByTagName("link").item(0);
+
+                        String details =
+                    }
+                }
+            }
+
+        } catch (MalformedURLException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (ParserConfigurationException e1) {
+            e1.printStackTrace();
+        }
+
+    }
 }
