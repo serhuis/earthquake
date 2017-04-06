0e9863354e21b228eab92dd18c2e3d73afa16b2f
serhiy-pc\serhiy <serhuis.s@gmail.com>
2017-04-06 19:14:46 +0300

    +2

--- app/src/main/java/com/example/serhiy/earthquake/EarthQuakeListFragment.java
+++ app/src/main/java/com/example/serhiy/earthquake/EarthQuakeListFragment.java
@@ -1,15 +1,18 @@
 package com.example.serhiy.earthquake;
 
 
+import android.location.Location;
 import android.os.Bundle;
-import android.provider.DocumentsContract;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
+import android.os.Handler;
+import android.app.Fragment;
+import android.app.ListFragment;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,11 +20,16 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
 
 
 /**
@@ -31,6 +39,8 @@ public class EarthQuakeListFragment extends ListFragment {
     ArrayAdapter<Quake> aa;
     ArrayList<Quake> earthquakes = new ArrayList<Quake>();
 
+    private static final String TAG = "EARTHQUAKE";
+    private Handler handler = new Handler();
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -42,11 +52,15 @@ public class EarthQuakeListFragment extends ListFragment {
 
         setListAdapter(aa);
 
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                refreshEarthQuakes();
+            }
+        });
+        t.start();
 
-    }
 
-    public EarthQuakeListFragment() {
-        // Required empty public constructor
     }
 
     public void refreshEarthQuakes() {
@@ -81,18 +95,58 @@ public class EarthQuakeListFragment extends ListFragment {
                         Element when = (Element)entry.getElementsByTagName("updated").item(0);
                         Element link = (Element)entry.getElementsByTagName("link").item(0);
 
-                        String details =
+                        String details = title.getFirstChild().getNodeValue();
+                        String hostname = "http://earthquake.usgs.gov";
+                        String linkString = hostname + link.getAttribute("href");
+
+                        String point = g.getFirstChild().getNodeValue();
+                        String dt = when.getFirstChild().getNodeValue();
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+                        Date qdate = new GregorianCalendar(0,0,0).getTime();
+
+                        try {
+                            qdate = sdf.parse(dt);
+                        } catch (ParseException e) {
+                            Log.d(TAG, "Date parsing exception.", e);
+                        }
+
+                        String[] location = point.split(" ");
+                        Location l = new Location("dummyGPS");
+                        l.setLatitude(Double.parseDouble(location[0]));
+                        l.setLongitude(Double.parseDouble(location[1]));
+
+                        String magnitudeString = details.split(" ")[1];
+                        int end = magnitudeString.length()-1;
+                        double magnitude = Double.parseDouble(magnitudeString.substring(0, end));
+
+                        details = details.split(",")[1].trim();
+
+                        final Quake quake = new Quake(qdate, details, l, magnitude, linkString);
+
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                addNewQuake(quake);
+                            }
+                        });
                     }
                 }
             }
 
         } catch (MalformedURLException e1) {
-            e1.printStackTrace();
+            Log.d(TAG, "MalformedURLException");
         } catch (IOException e1) {
-            e1.printStackTrace();
+            Log.d(TAG, "IOException");
         } catch (ParserConfigurationException e1) {
-            e1.printStackTrace();
+            Log.d(TAG, "ParserConfigurationException");
+        } catch (SAXException e) {
+            Log.d(TAG, "SAXException");
         }
 
+    }
+
+    private void addNewQuake(Quake _quake) {
+        earthquakes.add(_quake);
+        aa.notifyDataSetChanged();
     }
 }
